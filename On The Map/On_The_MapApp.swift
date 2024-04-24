1205f35c344813c97c8f76b3ff87c24e49acd9b1
KhuePM <minhkhuepham99@gmail.com>
2024-04-24 21:53:00 +0700

    Initial Commit

---
+++ On The Map/On_The_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  On_The_MapApp.swift
+//  On The Map
+//
+//  Created by KhuePM on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct On_The_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
